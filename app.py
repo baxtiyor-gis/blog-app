2b250d940918f07009112ec6755566ad8af242d9
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 19:34:37 +0500

    'Commited'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ file = open("file.txt","w")
 for i in range(1,10): 
     file.write("Time: %s"%time.ctime())   
     os.system("git add .")
-    os.system("git commit -m 'Commited at %s'"%time.ctime())
+    os.system("git commit -m 'Commited'")
     time.sleep(60)
  
 file.close()
