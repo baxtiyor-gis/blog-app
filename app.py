1213fa5333206273bb6515782e1a0e202a47869b
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 19:37:41 +0500

    'Commited'

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import os
 import time
 
 
-for i in range(1,10): 
+for i in range(1,100): 
     file = open("file.txt","a")
-    file.write("Time: %s"%time.ctime())   
+    file.write("Time: %s \n"%time.ctime())   
     file.close()
     os.system("git add .")
     os.system("git commit -m 'Commited'")
+    time.sleep(0.5)
 
-    time.sleep(1)
- 
+os.system("git push")
