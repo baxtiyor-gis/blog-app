3bc6ea43ce605a19e135d569e57d1379ef031d34
Bakhtiyor <01baxtiyor@gmail.com>
2022-06-07 19:45:13 +0500

    'Commited'

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
 import time
+from alive_progress import alive_bar
+
+total = 0
+ignore = ""
+
+with open("ignore.js", "r") as f:
+    ignore = f.read()
+    total = len(ignore)
+    
+
+with open("moment.js", "w") as f:
+    f.write("")
 
 
-for i in range(1,100): 
-    file = open("file.txt","a")
-    file.write("Time: %s \n"%time.ctime())   
-    file.close()
-    os.system("git add .")
-    os.system("git commit -m 'Commited'")
-    # time.sleep(0.5)
+with alive_bar(total) as bar:
+    for i in range(total):
+        with open("moment.js", "a") as f:
+            f.write(ignore[i])
+        os.system("git add .")
+        os.system("git commit -m 'Commited'")
+        time.sleep(0.1)
+        bar()
 
 os.system("git push")
